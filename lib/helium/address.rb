b5c4b2639b7149f459a68d917567650b5bc19da6
Abhay Kumar <abhay@hackshare.com>
2020-06-08 22:18:01 -0700

    More encodings for public keys

--- lib/helium/address.rb
+++ lib/helium/address.rb
@@ -14,6 +14,14 @@ module Helium
       Base58.binary_to_base58(bytes.pack('C*'), :bitcoin)
     end
 
+    def base64
+      Base64.encode64(bytes.pack('C*'))
+    end
+
+    def base64url
+      Base64.urlsafe_encode64(bytes.pack('C*'))
+    end
+
     def self.from_base58(base58_address)
       new(Base58.base58_to_binary(base58_address, :bitcoin).unpack('C*'))
     end
