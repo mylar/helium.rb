36fefbcbb0ba0531b241c02119ca2a01a0d01e65
Abhay Kumar <abhay@hackshare.com>
2021-04-02 15:59:20 -0700

    Squashed 'protos/' changes from 09f4ae5..76bf8db
    76bf8db Merge pull request #58 from helium/andymck/routersc_services
    a8bd96b Bump y18n from 4.0.0 to 4.0.1 (#70)
    d46df56 bump js version to 1.3.0 (#69)
    4b0a478 update js package version to 1.2.0 (#55)
    edee0f7 follow rpc should be bidectional stream
    9a06237 amalgamate routing and statechannel services into one new gateway proto
    1a08192 add state channel service
    71f6c38 fix node notifier version (#67)
    8e081fc Nanopb Build (#68)
    4565917 Add assert loc v2 proto (#41)
    ec3b6d0 Merge pull request #65 from helium/mra/rewards_v2
    5c7be1d Add rewards_v2 to top level txn
    3dcb80d Add rewards_v2 txn
    8374e7f Merge pull request #66 from helium/adt/consensus-group-failure-txn
    76aeeeb add unstake/transfer amounts for accounting
    9d76023 Add members field and add to txn proto
    073b0f7 Add a transaction to record election failures
    2c1f62e remove staking nonces (#64)
    788ea84 Merge pull request #63 from helium/adt/poc-addr-hash
    f41ce1d Merge pull request #62 from helium/madninja/no_val_sigs
    921c0ae No validator signatures
    da0244b Add addr hash field to poc receipt
    d0089d7 Merge pull request #61 from helium/madninja/validator_fixups
    5474cc3 Fixups for validator transactions
    b6923ad Add release.toml
    46b654d bump bytes dep
    2382920 Drop cargo.lock, bump deps
    f87ca4b Merge pull request #59 from helium/pevm/validators
    76d49cc validator txn protos
    2314d83 Fix rust build
    14b29c4 Merge pull request #60 from helium/andymck/rename-validator-to-gateway
    9a7ddcd rename validator service to gateway
    b79920f Merge pull request #56 from helium/madninja/services
    999c44a add router and validator grpc services
    
    git-subtree-dir: protos
    git-subtree-split: 76bf8db747472c098257c110626546f615823b37

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,14 @@
 include!(concat!(env!("OUT_DIR"), "/helium.rs"));
 pub use blockchain_txn::Txn;
 pub use prost::Message;
+#[cfg(feature = "services")]
+pub mod services {
+    pub mod router {
+        pub use crate::router_client::RouterClient as Client;
+    }
+    pub mod gateway {
+        pub use crate::gateway_client::GatewayClient as Client;
+    }
+
+    pub use tonic::transport::*;
+}
