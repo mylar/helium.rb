bef4cae41f1b1f7c19ddfa96b897713f103fb971
Abhay Kumar <abhay@opensynapse.net>
2021-11-06 20:46:37 -0700

    Squashed 'protos/' changes from 76bf8db..0b4fde8
    0b4fde8 Merge pull request #110 from helium/madninja/local_config
    70646b6 Add config local rpc request
    45a0edd Merge pull request #107 from helium/andymck/grpc-validators-req
    5031338 add validators grpc req
    560f183 Add helium.local for local gateway api service (#109)
    331ae0f attest validators block time and age (#108)
    9295883 Remove rustfmt in tonic-build dependency by default to allow production builds without rustfmt (#100)
    3349cee use strings for config keys (#106)
    19759a4 config service updates (#105)
    a2f8951 Adds state channel diffs to purchases (#98)
    648d7f7 Add RPC support to retrieve and monitor chain var updates (#103)
    9286e83 Merge pull request #104 from helium/madninja/proto_lint
    3994516 Format all protos
    774de63 Add basic proto lint format
    7ecd33f Fix CI (#102)
    9550395 Merge pull request #101 from helium/lthiery/pub-use-prost-errors
    c591a37 combine all prost imports on single line
    2139187 Rust: pub use prost::Encode/DecodeError
    ce0270c bump js version to 1.5.0 (#99)
    b25c996 Merge pull request #97 from helium/madninja/rejection_packet_hash
    aaa1bcc Add packet_hash to state channel rejection
    3a1f0a3 Merge pull request #94 from helium/pevm/hash-gossip
    53a1d37 Add gw nonce to transfer v2 proto (#95)
    aab39f8 add the ability to be more precise with what we're requesting
    dbf6fb7 add some fields to gossip proto
    a2856ee Merge pull request #91 from helium/rg/transfer-hotspot-v2
    7dcdbba Add transfer_hotspot_v2 txn proto
    1c89c47 Merge pull request #92 from helium/andymck/extend-isactive-api
    db85a16 add additional fields to is_active resp
    47446b2 Merge pull request #90 from helium/revert-89-andymck/remove-overpaid-grpc-rpc
    b647fe2 Revert "remove is_overpaid rpc"
    191d3fd Merge pull request #89 from helium/andymck/remove-overpaid-grpc-rpc
    52b1f6d remove is_overpaid rpc
    0068af3 add is_overpaid_sc and other misc tweaks (#88)
    390f21d Expose service StateChannelClient (#87)
    5577901 remove another service file from nanopb build (#73)
    87edb64 Bump hosted-git-info from 2.8.8 to 2.8.9 (#80)
    c20bf3c Bump path-parse from 1.0.6 to 1.0.7 (#85)
    78ce51a Bump ws from 7.2.3 to 7.5.3 (#86)
    e87ae25 Bump lodash from 4.17.19 to 4.17.21 (#79)
    56eb873 Spreading with packet size (#83)
    69a6452 Add region param proto (#77)
    0c5f792 Add AS923 sub-regions
    dd6f9ca Loosen rust deps to major versions
    e022ae1 Merge pull request #78 from helium/macpie/ch9765/router-instruct-hotspot-over-p2p-to-transmit
    b3cf7e2 Update packet to packetS
    529ca34 Rename txn to packet
    e201833 Add discovery mode message
    3d55f95 Merge pull request #76 from helium/macpie/ch9751/after-state-channel-outage-miner-sprays-packets
    ca01801 bump js version to 1.4.0 (#75)
    4a4f44e Merge pull request #72 from helium/rg/payment-v2-memos
    aad26fb Add hold_time to blockchain_state_channel_packet_v1
    b55d801 Add memo field to payment-v2
    85a5d31 Merge pull request #74 from helium/mra/unstake-cooldown
    0338787 Add stake_release_height field to unstake txn
    ac7821a keep gateway service in one service (#71)
    
    git-subtree-dir: protos
    git-subtree-split: 0b4fde8689cccbe0d49d968f4bdea49f7af3a96f

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,32 @@
 include!(concat!(env!("OUT_DIR"), "/helium.rs"));
+
 pub use blockchain_txn::Txn;
-pub use prost::Message;
+pub use prost::{DecodeError, EncodeError, Message};
+
 #[cfg(feature = "services")]
 pub mod services {
     pub mod router {
-        pub use crate::router_client::RouterClient as Client;
+        pub use crate::router_client::RouterClient;
+        pub use crate::state_channel_client::StateChannelClient;
     }
     pub mod gateway {
         pub use crate::gateway_client::GatewayClient as Client;
+    }
+
+    pub mod local {
+        include!(concat!(env!("OUT_DIR"), "/helium.local.rs"));
+        pub use api_client::ApiClient as Client;
+        pub use api_server::{Api, ApiServer as Server};
+
+        impl From<crate::BlockchainVarV1> for ConfigValue {
+            fn from(v: crate::BlockchainVarV1) -> Self {
+                Self {
+                    name: v.name,
+                    r#type: v.r#type,
+                    value: v.value,
+                }
+            }
+        }
     }
 
     pub use tonic::transport::*;
